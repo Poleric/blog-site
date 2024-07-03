eab32dbb02937ed4640ed421b07fe31e5402b906
Poleric <nixonleongkb@gmail.com>
2024-07-03 23:15:12 +0800

    get prod working

--- apps/blogsite/settings/base.py
+++ apps/blogsite/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+env = os.environ.copy()
+
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
--- apps/blogsite/settings/production.py
+++ apps/blogsite/settings/production.py
@@ -1,6 +1,14 @@
 from .base import *
 
+from socket import gethostbyname
+from socket import gethostname
+
 DEBUG = False
+
+SECRET_KEY = env["SECRET_KEY"]
+
+ALLOWED_HOSTS = env.get("ALLOWED_HOST").split(",")
+ALLOWED_HOSTS.append(gethostbyname(gethostname()))
 
 try:
     from .local import *
