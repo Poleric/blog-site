221f38cf4c8125882230e2979d0620c5a4dce5e8
Poleric <nixonleongkb@gmail.com>
2024-07-08 14:32:28 +0800

    add image to author

--- apps/blog/models/snippets.py
+++ apps/blog/models/snippets.py
@@ -7,9 +7,17 @@ from wagtail.snippets.models import register_snippet
 @register_snippet
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    author_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     panels = [
-        FieldPanel("name")
+        FieldPanel("name"),
+        FieldPanel('author_image')
     ]
 
     def __str__(self):
